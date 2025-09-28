6df56802d67cb4c936a2a7ac8ac6ce9380638f51
dherrerahe <dherrerahe@unal.edu.co>
2025-09-27 21:25:16 -0500

    Inicio Front-End con React
    Se empieza trabajar en el proyecto usando React.

---
+++ kairosyndes/src/Components/SegundoComponente.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+let nombre = 'David';
+
+export const SegundoComponente = () => {
+  return (
+    <div>
+        SegundoComponente
+        <h1>Nombre: {nombre}</h1>
+    </div>
+  )
+}
